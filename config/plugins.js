6809a8266fd7a746c9d7ea9443b44a1d171016fd
codingbear <maxswjeon@planetariumhq.com>
2023-03-05 04:34:16 +0900

    fix: env vars

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,14 @@ module.exports = ({ env }) => ({
   "strapi-plugin-keycloak": {
     enabled: true,
     config: {
-      KEYCLOAK_SERVER_URL: process.env.KEYCLOAK_SERVER_URL,
-      KEYCLOAK_REALM: process.env.KEYCLOAK_REALM,
-      KEYCLOAK_CLIENT_ID: process.env.KEYCLOAK_CLIENT_ID,
-      KEYCLOAK_CLIENT_SECRET: process.env.KEYCLOAK_CLIENT_SECRET,
-      KEYCLOAK_REDIRECT_URL: `${process.env.PUBLIC_URL}/strapi-plugin-keycloak/keycloak/callback`,
+      KEYCLOAK_SERVER_URL: env("KEYCLOAK_SERVER_URL"),
+      KEYCLOAK_REALM: env("KEYCLOAK_REALM"),
+      KEYCLOAK_CLIENT_ID: env("KEYCLOAK_CLIENT_ID"),
+      KEYCLOAK_CLIENT_SECRET: env("KEYCLOAK_CLIENT_SECRET"),
+      KEYCLOAK_REDIRECT_URL: `${env(
+        "STRAPI_URL",
+        "http://localhost:1337"
+      )}/strapi-plugin-keycloak/keycloak/callback`,
     },
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("STRAPI_HOST", "0.0.0.0"),
+  port: env.int("STRAPI_PORT", 1337),
+  url: env("STRAPI_URL", "http://localhost:1337"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("STRAPI_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
